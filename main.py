fcfc09f7a56e995b83c602a61451c52461c9bdcc
Akash Gaur <134853842+akgaur12@users.noreply.github.com>
2023-06-18 10:29:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ import pytube
 from pytube import YouTube
 
 bg_color = "#D7D7D7"
+
+root = Tk()
+root.title("YouTube Video Downloader")
+root.geometry("600x650+500+50")
+root.iconbitmap("logo.ico")
+root.resizable(0,0)
+root.config(bg=bg_color)
